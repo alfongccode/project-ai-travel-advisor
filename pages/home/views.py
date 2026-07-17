13106fa7981813ac48109afc3acda6388ae644ab
Alfonso García <agcortijo@gmail.com>
2026-07-17 04:52:02 +0200

    fix(home): fixed context sessions bug

--- pages/home/views.py
+++ pages/home/views.py
@@ -2,10 +2,15 @@ from django.shortcuts import render
 from main import TravelAdvisor
 
 ACTIONS_CLEAR = "clear"
-travel_advisor = TravelAdvisor().initialize_client_session()
+
+def get_travel_advisor(request):
+    advisor = TravelAdvisor().initialize_client_session()
+    advisor.context = request.session.get('travel_context', advisor.context)
+    return advisor
 
 def home_view(request):
     response = {}
+    travel_advisor = get_travel_advisor(request)
     if request.method == "POST":
         action = request.POST.get("action", None)
         user_request = request.POST.get("request", None)
@@ -17,5 +22,6 @@ def home_view(request):
                 response = arguments
                 response['itinerary_map'] = travel_advisor.generate_itinerary_image(arguments['itinerary'])
                 response['photographs'] = travel_advisor.get_photographs(arguments['trip_location'])
+            request.session['travel_context'] = travel_advisor.context
             return render(request, "index.html", { "history": travel_advisor.get_answers_history(), "response": response })
     return render(request, "index.html")
