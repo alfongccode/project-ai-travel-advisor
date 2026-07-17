873943a50d6959694245d9625dc8159982513f4d
Alfonso García <agcortijo@gmail.com>
2026-07-17 05:13:12 +0200

    chore(): rollback context system

--- core/settings.py
+++ core/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 # Vercel expone automáticamente esta variable con el dominio del deployment actual
 VERCEL_URL = os.environ.get("VERCEL_URL")
--- pages/home/views.py
+++ pages/home/views.py
@@ -1,19 +1,16 @@
+import json
 from django.shortcuts import render
 from main import TravelAdvisor
 
 ACTIONS_CLEAR = "clear"
-
-def get_travel_advisor(request):
-    advisor = TravelAdvisor().initialize_client_session()
-    advisor.context = request.session.get('travel_context', advisor.context)
-    return advisor
+travel_advisor = TravelAdvisor().initialize_client_session()
 
 def home_view(request):
     response = {}
-    travel_advisor = get_travel_advisor(request)
     if request.method == "POST":
         action = request.POST.get("action", None)
         user_request = request.POST.get("request", None)
+
         if action == ACTIONS_CLEAR:
             travel_advisor.clear()
         if user_request:
@@ -22,6 +19,5 @@ def home_view(request):
                 response = arguments
                 response['itinerary_map'] = travel_advisor.generate_itinerary_image(arguments['itinerary'])
                 response['photographs'] = travel_advisor.get_photographs(arguments['trip_location'])
-            request.session['travel_context'] = travel_advisor.context
             return render(request, "index.html", { "history": travel_advisor.get_answers_history(), "response": response })
     return render(request, "index.html")
